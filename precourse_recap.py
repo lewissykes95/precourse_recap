fc48c2fb2b0e0aa17374fc3738e9b8ce48d8436e
Lewis <lewisdsykes@gmail.com>
2022-06-21 09:22:57 +0100

    3rd Commit - made a small change to the codeclan_student variable

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,6 +1,6 @@
 name = "lewis"
 age  = 20 + 6
-codeclan_student = True
+codeclan_student = False
 
 name = name.capitalize()
 
@@ -9,7 +9,9 @@ print("My name is " + name + " and I am " + str(age) + " years old")
 if codeclan_student == True:
     print("I am a CodeClan Student")
 else:
-    print("I am not a CodeClan")
+    print("I am not a CodeClan Student")
+
+
 
 
 
