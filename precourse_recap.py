3eaec916611eb9f9884c735c0ec4d33d296dc1bb
Lewis <lewisdsykes@gmail.com>
2022-06-21 09:19:24 +0100

    2nd commit and added if statement

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,10 +1,15 @@
 name = "lewis"
 age  = 20 + 6
-CodeClan_student = True
+codeclan_student = True
 
 name = name.capitalize()
 
 print("My name is " + name + " and I am " + str(age) + " years old")
+
+if codeclan_student == True:
+    print("I am a CodeClan Student")
+else:
+    print("I am not a CodeClan")
 
 
 
